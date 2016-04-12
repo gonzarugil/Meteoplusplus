e3be79c588089c04ebeb8fa248ec65f7b8553872
gonzarugil <gonzarugil@gmail.com>
2016-04-12 10:26:02 +0200

    cosas

--- arduino/arduino_code/arduino_code.ino
+++ arduino/arduino_code/arduino_code.ino
@@ -1,16 +1,23 @@
 #include <LiquidCrystal.h>//Incluimos la libreria de la LCD
 #include "DHT.h"//Incluye libraría de control del sensor
-#define DHTPIN 8//Defiene el pin al que se conectará el sensor
+#define DHTPIN 8 //Defiene el pin al que se conectará el sensor
+
 #define DHTTYPE DHT11//Seleciona el tipo de sensor
 DHT dht(DHTPIN, DHTTYPE);//Configura la libraría
-
+int analogPin=0;//Declaramos que queremos usar el pin analógico 0
+int analogPinb=2;//Declaramos que queremos usar el pin analógico 2
 int Humedad=0;//Declaramos la variable Humedad
-int Temperatura=0;//Declaramos la variable Humedad
-
+int Temperatura=0;//Declaramos la variable Temperatura
+int Suelo=0;//Declaramos la variable Humedad Suelo
+int Valor=0;//Declaramos porcentaje humedad suelo
+int Auxiliar=0;//Declaramos auxiliar para humedad suelo
+int Agua=0;
+int AguaAux = 0;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);//Declaramos los pines que usa nuestra pantalla(RS,E,D4,D5,D6,D7)
 
 void setup() {
   dht.begin();//Inicializa la libraria dht
+  Serial.begin(9600);
   lcd.begin(16, 2);//Iniciamos la pantalla y le decimos el numero de caracteres y filas
   
   lcd.setCursor(4, 0);//Situamos el lugar donde empezará la escritura
@@ -45,6 +52,33 @@ void loop() {
   lcd.print(Temperatura);//Representamos el valor de la variable Temperatura.
   lcd.setCursor(14, 1);//Situamos el lugar donde empezará la escritura
   lcd.print("C");//Escribimos C en la LCD
-  delay(100);//Esperamos 100 milisegundos
+  delay(3000);//Esperamos 100 milisegundos
+  lcd.clear();//limpiamos la pantalla
+  lcd.setCursor(0, 0);//Situamos el lugan donde empezará la escritura
+  Suelo=analogRead(analogPin);
+  Auxiliar=Suelo/10,24;
+  Valor=100-Auxiliar;
+  if(Valor<0)
+    Valor=0;
+  lcd.print("HUMEDAD SUELO");
+  lcd.setCursor(3,1);
+  lcd.print(Valor);
+  //lcd.setCursor(6,1);
+  lcd.print("%");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  Agua=analogRead(analogPinb);
+  lcd.print("NIVEL DE AGUA");
+  lcd.setCursor(0,1);
+  AguaAux=Agua/4;
+  lcd.print(Agua);
+  lcd.print("%");
   
+  //Serial.print(Agua);
+  delay(3000);
+  lcd.clear();
+  
+  
+ 
     }
--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 # Import Libraries
 
-import twitter
-import serial
-import time
+import tweepy
 
-api = twitter.Api(consumer_key='xOca0pffD13sxRvY0mTdR9Kef',
-                  consumer_secret='T62Pl6IpqJ9MkvHEIjf7ooNG2yqZODQ1c5dFijbxzfPRPQxNS5',
-                  access_token_key='716272805042601986-Is7aB8RKV8DaLp8WqMbJmSH9CpFd2ze',
-                  access_token_secret='TrZ62MZXW7dE1AZj0vUP4A6m2cipUEvunWzsKT2xH1epr')
+# api = twitter.Api(consumer_key='xOca0pffD13sxRvY0mTdR9Kef',
+#                  consumer_secret='T62Pl6IpqJ9MkvHEIjf7ooNG2yqZODQ1c5dFijbxzfPRPQxNS5',
+#                  access_token_key='716272805042601986-Is7aB8RKV8DaLp8WqMbJmSH9CpFd2ze',
+#                  access_token_secret='TrZ62MZXW7dE1AZj0vUP4A6m2cipUEvunWzsKT2xH1epr')
+
+
+auth = tweepy.OAuthHandler('xOca0pffD13sxRvY0mTdR9Kef', 'T62Pl6IpqJ9MkvHEIjf7ooNG2yqZODQ1c5dFijbxzfPRPQxNS5')
+auth.set_access_token('716272805042601986-Is7aB8RKV8DaLp8WqMbJmSH9CpFd2ze', 'TrZ62MZXW7dE1AZj0vUP4A6m2cipUEvunWzsKT2xH1epr')
+
+api = tweepy.API(auth)
+
+mentions = api.mentions_timeline()
+for tweet in mentions:
+    list = tweet.text.split()
+    if "#weather" in list:
+        print("i'll check replies")
+        id = tweet.id
+        sn = tweet.user.screen_name
+        replies = api.search(q=sn, sinceId=id)
+        found = False
+        for e in replies:
+            if e.in_reply_to_status_id == id:
+                found = True
+                break
+        if not found:
+            # This is where we ask arduino for the data
+            
+            # ------------------------------------------
+            reply = "@%s Hello again!" % sn
+            api.update_status(reply,tweet.id)
 
 # Welcome message
-print('Welcome To Meteo ++!')
 
-status = []
-x = 0
-status = api.GetUserTimeline(screen_name='gonzarugil')  # grab latest statuses
-checkIt = [s.text for s in status]  # put status in an array
-
-
-print(checkIt[0])
